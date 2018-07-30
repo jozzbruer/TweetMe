fb30456bbd1da311c9ad658221bd3da0f45dfc79
The German <quincejozbruer@gmail.com>
2018-07-29 23:25:17 -0400

    Profile setup final

--- app/src/main/java/com/codepath/apps/restclienttemplate/ProfileActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ProfileActivity.java
@@ -3,7 +3,10 @@ package com.codepath.apps.restclienttemplate;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.codepath.apps.restclienttemplate.fragments.UserTimelineFragment;
 import com.codepath.apps.restclienttemplate.models.User;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -41,11 +44,35 @@ public class ProfileActivity extends AppCompatActivity {
                     User user = User.fromJSON(response);
                     // Set the title of the ActionBar base on the user information
                     getSupportActionBar().setTitle(user.screenName);
+                    // Populate the user headline
+                    populateUserHeadline(user);
                 }catch(JSONException e){
                     e.printStackTrace();
                 }
             }
         });
+
+    }
+
+    public void populateUserHeadline(User user){
+        TextView tvName = findViewById(R.id.tvName);
+        TextView tvTagline = findViewById(R.id.tvTagline);
+        TextView tvFollowers = findViewById(R.id.tvFollowers);
+        TextView tvFollowing = findViewById(R.id.tvFollowing);
+
+        ImageView ivProfileImage = findViewById(R.id.ivProfilImage);
+
+        tvName.setText(user.name);
+        tvTagline.setText(user.tagLine);
+        tvFollowers.setText(user.followersCount + " Followers");
+        tvFollowing.setText(user.followingCount + " Following");
+        //
+
+        // Load Profile Image with Glide
+        Glide.with(this).load(user.profileImageUrl).into(ivProfileImage);
+
+
+
 
     }
 }
